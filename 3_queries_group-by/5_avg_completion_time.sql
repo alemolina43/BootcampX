e1936cdbe97832a0b2bb2fddf228d6c89eae1ef2
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-18 12:30:34 -0700

    Create tables teachers and assistance_requests and add data to them

--- 3_queries_group-by/5_avg_completion_time.sql
+++ 3_queries_group-by/5_avg_completion_time.sql
@@ -1,6 +1,6 @@
-SELECT students.name AS student, SUM(assignment_submissions.duration) AS average_assignment_duration
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-WHERE students.end_date IS NULL
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
 GROUP BY student
 ORDER BY average_assignment_duration DESC;
